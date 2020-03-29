ded7746a97c65106b55c8f87a4fdd832c3a316b7
Mateaus <mateausc@gmail.com>
2020-03-29 02:12:23 -0500

    Fixed merge conflicts

--- covid/views.py
+++ covid/views.py
@@ -3,25 +3,16 @@ import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse, HttpResponse
-<<<<<<< HEAD
-from .models import allCases, state, county
-from .serializers import AllCaseSerializer, AllStatesSerializer, AllCountySerializer
-=======
 from .models import allCases, state, county, countries
 from .serializers import AllCaseSerializer, AllStatesSerializer, AllCountySerializer, AllCountrySerializer
->>>>>>> master
 
 # Create your views here.
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
         'all-cases': 'cases/',
-<<<<<<< HEAD
-        'country': '/country/<str:pk>/',
-=======
         'all-single-country-cases': '/country/<str:pk>/',
         'all-countries-cases': '/countries/',
->>>>>>> master
         'all-states': '/states/',
         'state': '/state/<str:pk>/',
         'all-county': '/state/<str:fk>/counties',
@@ -63,8 +54,6 @@ def state_county_cases(request, fk, pk):
     all_county_cases = county.objects.filter(state_name=fk).get(county_name=pk)
     serializer = AllCountySerializer(all_county_cases, many=False)
     return Response(serializer.data)
-<<<<<<< HEAD
-=======
 
 
 @api_view(['GET'])
@@ -79,4 +68,3 @@ def single_country_cases(request, pk):
     all_country_cases = countries.objects.get(country_name=pk)
     serializer = AllCountrySerializer(all_country_cases, many=False)
     return Response(serializer.data)
->>>>>>> master
