c1e000d9b6b394aab97568167bbcf90922d04ec8
Charles-Garza <41405572+Charles-Garza@users.noreply.github.com>
2020-03-29 02:11:12 -0500

    Adding endpoint to return all counties

--- covid/views.py
+++ covid/views.py
@@ -3,29 +3,25 @@ import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.http import JsonResponse, HttpResponse
-<<<<<<< HEAD
 from .models import allCases, state, county
 from .serializers import AllCaseSerializer, AllStatesSerializer, AllCountySerializer
-=======
 from .models import allCases, state, county, countries
 from .serializers import AllCaseSerializer, AllStatesSerializer, AllCountySerializer, AllCountrySerializer
->>>>>>> master
+
 
 # Create your views here.
 @api_view(['GET'])
 def apiOverview(request):
     api_urls = {
         'all-cases': 'cases/',
-<<<<<<< HEAD
         'country': '/country/<str:pk>/',
-=======
         'all-single-country-cases': '/country/<str:pk>/',
         'all-countries-cases': '/countries/',
->>>>>>> master
         'all-states': '/states/',
         'state': '/state/<str:pk>/',
         'all-county': '/state/<str:fk>/counties',
         'county': '/state/<str:fk>/county/<str:pk>/',
+        'all-counties': '/counties/all',
     }
 
     return Response(api_urls)
@@ -52,7 +48,7 @@ def state_cases(request, pk):
 
 
 @api_view(['GET'])
-def every_county_case(request, fk):
+def every_state_county_case(request, fk):
     all_county_cases = county.objects.filter(state_name=fk).all()
     serializer = AllCountySerializer(all_county_cases, many=True)
     return Response(serializer.data)
@@ -63,9 +59,13 @@ def state_county_cases(request, fk, pk):
     all_county_cases = county.objects.filter(state_name=fk).get(county_name=pk)
     serializer = AllCountySerializer(all_county_cases, many=False)
     return Response(serializer.data)
-<<<<<<< HEAD
-=======
 
+
+@api_view(['GET'])
+def all_county_cases(request):
+    all_county_cases = county.objects.all()
+    serializer = AllCountySerializer(all_county_cases, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def all_country_cases(request):
@@ -79,4 +79,4 @@ def single_country_cases(request, pk):
     all_country_cases = countries.objects.get(country_name=pk)
     serializer = AllCountrySerializer(all_country_cases, many=False)
     return Response(serializer.data)
->>>>>>> master
+
--- scheduler/updater.py
+++ scheduler/updater.py
@@ -1,10 +1,6 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from scheduler import casesAPI
 
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(casesAPI.gather_all_info, 'interval', hours=3)
