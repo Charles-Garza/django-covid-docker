8b260b47008a9be85886448a9c4f3f985aed768c
Charles-Garza <41405572+Charles-Garza@users.noreply.github.com>
2020-03-29 01:54:13 -0500

    Fixing time for scheduler

--- scheduler/updater.py
+++ scheduler/updater.py
@@ -4,5 +4,5 @@ from scheduler import casesAPI
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(casesAPI.gather_all_info, 'interval', minutes=15)
+    scheduler.add_job(casesAPI.gather_all_info, 'interval', hours=3)
     scheduler.start()
